5d61ec4c47cd4d82a191c775448958a1fdb541e9
Екатерина <76491846+EkaterinaPeregudova@users.noreply.github.com>
2021-01-19 10:14:15 +0300

    Update and rename Код.java to Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         int tekets = 2500;
         int mili = 20;
         int rest = tekets / mili;
         System.out.print(rest);
     }
- }
+}
